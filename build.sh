0882a3d0249b51724f7b477bddfaced8085c8276
Huy Truong <nguyenhuytruong9112k@gmail.com>
2025-08-14 10:37:25 +0700

    update

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 412381763978.dkr.ecr.ap-southeast-1.amazonaws.com
+aws ecr get-login-password --region eu-south-1 | \
+docker login --username AWS --password-stdin 412381763978.dkr.ecr.eu-south-1.amazonaws.com
 
 docker build -t my-game-server .
 
-docker tag my-game-server:latest 412381763978.dkr.ecr.ap-southeast-1.amazonaws.com/containers/tressette:latest
+docker tag my-game-server:latest 412381763978.dkr.ecr.eu-south-1.amazonaws.com/containers/tressette:latest
 
-docker push 412381763978.dkr.ecr.ap-southeast-1.amazonaws.com/containers/tressette:latest
+docker push 412381763978.dkr.ecr.eu-south-1.amazonaws.com/containers/tressette:latest
 
